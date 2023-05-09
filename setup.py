3cea4990b611a74a087e94f289d9568508b04190
Chris Davis <ChrisSWDavis@gmail.com>
2023-05-09 20:43:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     package_data={"":["CosRayModifiedISO/neutronMonitorData/*.dat","CosRayModifiedISO/neutronMonitorData/*.pkl"]},
     include_package_data=True,
-    version='1.0',
+    version='1.0.1',
     url="https://github.com/ssc-maire/CosRayModifiedISO",
     author='Chris S. W. Davis',
     author_email='ChrisSWDavis@gmail.com',
